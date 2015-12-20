ed89dc2a00bb120fcfae83e57ec3004fad98e8fb
Braden Walters <vc@braden-walters.info>
2015-12-20 03:54:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Dresdner Verkehrsbetriebe (DVB) Haskell Program and Library
+
+A library and utility program for querying the DVB (Dresdner Verkehrsbetriebe)
+API.
---
+++ src/Network/Transportation/Germany/DVB.hs
@@ -0,0 +1,9 @@
+-- |
+-- Module: Network.Transportation.Germany.DVB
+-- Copyright: (C) 2015 Braden Walters
+-- License: MIT (see LICENSE file)
+-- Maintainer: Braden Walters <vc@braden-walters.info>
+-- Stability: experimental
+-- Portability: ghc
+
+module Network.Transportation.Germany.DVB where
